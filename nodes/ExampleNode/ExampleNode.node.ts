2d6065dc670f9e8987046ff6258488e1ceaa8428
ashokfacto <66980305+ashokfacto@users.noreply.github.com>
2023-06-13 04:11:26 +0000

    test c1

--- nodes/ExampleNode/ExampleNode.node.ts
+++ nodes/ExampleNode/ExampleNode.node.ts
@@ -6,15 +6,15 @@ import {
 	NodeOperationError,
 } from 'n8n-workflow';
 
-export class ExampleNode implements INodeType {
+export class mdmtestNode implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Example Node',
-		name: 'exampleNode',
+		displayName: 'MDMTEST Node',
+		name: 'mdmtestNode',
 		group: ['transform'],
 		version: 1,
-		description: 'Basic Example Node',
+		description: 'Basic MDMTEST Node',
 		defaults: {
-			name: 'Example Node',
+			name: 'MDMTEST Node',
 		},
 		inputs: ['main'],
 		outputs: ['main'],
@@ -22,35 +22,35 @@ export class ExampleNode implements INodeType {
 			// Node properties which the user gets displayed and
 			// can change on the node.
 			{
-				displayName: 'My String',
-				name: 'myString',
+				displayName: 'Mdm Contact',
+				name: 'mdmContact',
 				type: 'string',
 				default: '',
-				placeholder: 'Placeholder value',
-				description: 'The description text',
+				placeholder: 'Placeholder value for MDM Test Node',
+				description: 'The description text for MDM Test Node',
 			},
 		],
 	};
 
 	// The function below is responsible for actually doing whatever this node
-	// is supposed to do. In this case, we're just appending the `myString` property
+	// is supposed to do. In this case, we're just appending the `mdmContact` property
 	// with whatever the user has entered.
 	// You can make async calls and use `await`.
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
 
 		let item: INodeExecutionData;
-		let myString: string;
+		let mdmContact: string;
 
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
+		// Iterates over all input items and add the key "mdmContact" with the
+		// value the parameter "mdmContact" resolves to.
 		// (This could be a different value for each item in case it contains an expression)
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
+				mdmContact = this.getNodeParameter('mdmContact', itemIndex, '') as string;
 				item = items[itemIndex];
 
-				item.json['myString'] = myString;
+				item.json['mdmContact'] = mdmContact;
 			} catch (error) {
 				// This node should never fail but we want to showcase how
 				// to handle errors.
